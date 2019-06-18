9fe9cb90a9049a6e4525246f6f9db6aa08e64ab9
Garrett Thompson <gtcruz.gt@gmail.com>
2019-06-18 02:27:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,6 +13,11 @@ def display_board(board)
 end
 
 #is valid move method
-def valid_move?
-
+def valid_move?(board, index)
+  index = index.to_i - 1
+    if !position_taken?(board, index) && index.between(0,8)
+      true
+    else
+      false
+    end
 end
