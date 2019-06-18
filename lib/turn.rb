edb9c3e10ab1f5e807541ec4ac7b3fe616e9766f
Garrett Thompson <gtcruz.gt@gmail.com>
2019-06-18 02:21:24 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+#turn method
+def turn
+
+end
+
+#board method
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#is valid move method
+def valid_move?
+
+end
